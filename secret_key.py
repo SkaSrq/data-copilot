50bc90d414d4233eb694242e8d4fe6c1e8840646
Sharique <shariqueaman28@gmail.com>
2023-08-11 19:49:14 +0530

    secret key changed

--- secret_key.py
+++ secret_key.py
@@ -1,2 +1,2 @@
-openai_key="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"
+openai_key="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"
 serp_api_key="6ee564682061ed4d2b9fc8e076e320e605ee52aca359901a12e4791ad8a6a882"
