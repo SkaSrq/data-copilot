2681811fdc316af316e75a51df19fef1c098494c
Sharique <shariqueaman28@gmail.com>
2023-08-14 13:55:22 +0530

    my commit

--- secret_key.py
+++ secret_key.py
@@ -1,2 +1,2 @@
-openai_key="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"
+openai_key="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"
 serp_api_key="6ee564682061ed4d2b9fc8e076e320e605ee52aca359901a12e4791ad8a6a882"
